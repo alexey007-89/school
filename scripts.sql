8566fec379a9a39383af4e6d413af1bdd6952146
Aleksey Pakhomov <Alexey007-89@mail.ru>
2022-02-28 21:27:48 +1000

    3.5. Потоки данных. Работа с файлами.

--- scripts.sql
+++ scripts.sql
@@ -2,18 +2,11 @@ select *
 from student
 where age >= 8 and age <= 10;
 
-select name
-from student;
+select name from student;
 
-select *
-from student
-where name like '%a%';
+select * from student where lower (name) like '%b%';
 
 
-select *
-from student
-where age < student.id;
+select * from student where age < student.id;
 
-select *
-from student
-order by age;
+select * from student order by age;
