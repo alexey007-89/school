f3f7f004ee7620308911443c8ab3e83f34868671
Aleksey Pakhomov <Alexey007-89@mail.ru>
2022-03-16 08:38:51 +1000

    4.2. Управление схемами баз данных

--- scripts422.sql
+++ scripts422.sql
@@ -1,12 +1,12 @@
 CREATE TABLE persons (
-    id SERIAL,
+    id SERIAL PRIMARY KEY,
     name TEXT,
     driver_license BOOLEAN,
     car_id INTEGER REFERENCES cars(id)
 );
 
 CREATE TABLE cars (
-    id SERIAL,
+    id SERIAL PRIMARY KEY,
     manufacturer TEXT ,
     model TEXT,
     price INTEGER CHECK ( price > 0 )
